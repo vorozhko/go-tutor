4599350e776a8bc16dc700a4607a804b46ae041b
Yaroslav Vorozhko <yaroslav.vorozhko@deliveryhero.com>
2017-11-13 00:44:16 +0100

    simple grep program

--- day2/README.md
+++ day2/README.md
@@ -1,1 +1,4 @@
 Search for a string in a given filepath 
+
+- Input file path and string to search
+- Output file names which contain the string and position
--- day2/grep.go
+++ day2/grep.go
@@ -3,10 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	filepath := flag.String("filepath", "", "file path to search in")
+	path := flag.String("path", "", "file path to search in")
+	search := flag.String("search", "", "search string to look for")
 	flag.Parse()
-	fmt.Printf("Search in... %s\n", *filepath)
+
+	files, err := ioutil.ReadDir(*path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+
+		data, err := ioutil.ReadFile(file.Name())
+		fileType := http.DetectContentType(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			if strings.Index(line, *search) > -1 {
+				if strings.Index(fileType, "text/plain") > -1 {
+					fmt.Printf("%s: %s\n", file.Name(), line)
+				} else {
+					fmt.Printf("Binary file %s matches\n", file.Name())
+				}
+
+			}
+		}
+	}
 }
