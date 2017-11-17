3ed439131bb1ecbca3fd8f9bfbb1fc8e3cbad218
Yaroslav Vorozhko <yaroslav.vorozhko@deliveryhero.com>
2017-11-17 17:25:49 +0100

    visit each link only once; exclude external links

--- day5/fetcher.go
+++ day5/fetcher.go
@@ -13,29 +13,37 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strings"
 
 	"golang.org/x/net/html"
 )
 
+var visitedLinks map[string]bool
+var baseURL = flag.String("url", "", "start url")
+
 func main() {
-	url := flag.String("url", "", "start url")
+	visitedLinks = make(map[string]bool)
+
 	//timeout := flag.Int("timeout", 2, "number of seconds between requests")
 	flag.Parse()
 
-	if *url == "" {
+	if *baseURL == "" {
 		log.Fatal("--url paramters is required")
 	}
 
-	crawl(*url, 0, 3, 5)
+	visitedLinks[*baseURL] = false
+	crawl("/", 0, 10, 10)
 }
 
-func crawl(url string, currentDepth, finalDepth, maxLinks int) {
+func crawl(link string, currentDepth, finalDepth, maxLinks int) {
+	if visitedLinks[link] {
+		return
+	}
+	visitedLinks[link] = true
 	if currentDepth == finalDepth {
 		return
 	}
-	fmt.Printf("Crawling %s ..................\n\n", url)
-	resp, err := http.Get(url)
+	fmt.Printf("Crawling %s ..................\n\n", *baseURL+link)
+	resp, err := http.Get(*baseURL + link)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,16 +51,19 @@ func crawl(url string, currentDepth, finalDepth, maxLinks int) {
 
 	linkCounter := 0
 	//todo: every link must be visited only once
-	for _, link := range getLinks(resp.Body) {
+	for _, href := range getLinks(resp.Body) {
 		//crawl only internal links
-		if string(link[0]) == "/" && // only internal links
-			strings.Index(url, link) == -1 { //skip current page
+		if string(href[0]) == "/" && // only internal links
+			href != link { //skip current page
+			if len(href) > 1 && href[1] == '/' { //skip external links which start with //
+				continue
+			}
 			if linkCounter == maxLinks {
 				return
 			}
 			linkCounter++
-			fmt.Printf("Found: %s\n", link)
-			crawl(url+link, currentDepth+1, finalDepth, maxLinks)
+			//fmt.Printf("Found: %s\n", href)
+			crawl(href, currentDepth+1, finalDepth, maxLinks)
 		}
 	}
 }
